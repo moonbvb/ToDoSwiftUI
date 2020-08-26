66a7e38776df87834e634781575e2c7e0222c8e1
Denis Sergeevich <denissuspicin@gmail.com>
2020-08-26 22:13:56 +0300

    add state with list items.lol

--- ToDoSwiftUI/ContentView.swift
+++ ToDoSwiftUI/ContentView.swift
@@ -9,8 +9,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var checklistItems = [
+        "Walk the dog",
+        "Brush my teeth",
+        "Learn iOS development",
+        "Soccer practice",
+        "Eat ice cream",
+        "Learn every martial art",
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                Text(checklistItems[0])
+                    .onTapGesture {
+                        self.checklistItems[0] = "Take the dog to the vet"
+                }
+                Text(checklistItems[1])
+                Text(checklistItems[2])
+                Text(checklistItems[3])
+                Text(checklistItems[4])
+            }
+            .navigationBarTitle("Checklist")
+        }
     }
 }
 
